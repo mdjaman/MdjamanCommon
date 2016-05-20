3d06eb57273002e0fa96275c135a73dac38a2028
Marcel Djaman <marceldjaman@gmail.com>
2016-05-20 13:54:29 +0000

    Added onFlush event

--- src/MdjamanCommon/EventListener/DoctrineExtensionsListener.php
+++ src/MdjamanCommon/EventListener/DoctrineExtensionsListener.php
@@ -33,60 +33,93 @@ class DoctrineExtensionsListener
 
     protected $authServiceName;
     
-    
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param string $authSceName
+     */
     public function __construct(ServiceLocatorInterface $serviceLocator, $authSceName = 'zfcuser_auth_service')
     {
         $this->sl = $serviceLocator;
         $this->authServiceName = $authSceName;
     }
 
+    /**
+     * @param EventArgs $event
+     * @return void
+     */
     public function preFlush(EventArgs $event)
     {
-        if (!$this->sl->has($this->authServiceName)) {
-            return;
-        }
-        
-        $authenticationService = $this->sl->get($this->authServiceName);
-        if (!$authenticationService instanceof AuthenticationService) {
-            return;
-        }
-
-        if ($authenticationService->hasIdentity()) {
-            $identity = $authenticationService->getIdentity();
-            
-            if (method_exists($event, 'getEntityManager')) {
-                $objectManager = $event->getEntityManager();
+        return $this->onEvent($event);
+    }
+    
+    /**
+     * @param EventArgs $event
+     * @return void
+     */
+    public function onFlush(EventArgs $event)
+    {
+        return $this->onEvent($event);
+    }
+    
+    /**
+     * @param EventArgs $event
+     * @return void
+     */
+    protected function onEvent(EventArgs $event)
+    {
+        try {
+            if (is_string($this->authServiceName)) {
+                if (!$this->sl->has($this->authServiceName)) {
+                    throw new \Exception(sprintf('Couldn\'t find Service %s', $this->authServiceName));
+                }
+                $authenticationService = $this->sl->get($this->authServiceName);
             } else {
-                $objectManager = $event->getDocumentManager();
+                $authenticationService = $this->authServiceName;
             }
-            
-            $evtManager = $objectManager->getEventManager();
-            foreach ($evtManager->getListeners() as $listeners) {
-                foreach ($listeners as $listener) {
-                    if ($listener instanceof BlameableListener) {
-                        $this->blameableListener = $listener;
 
-                        continue;
-                    }
-                    if ($listener instanceof LoggableListener) {
-                        $this->loggableListener = $listener;
+            if (!$authenticationService instanceof AuthenticationService) {
+                return;
+            }
 
-                        continue;
+            if ($authenticationService->hasIdentity()) {
+                $identity = $authenticationService->getIdentity();
+
+                if (method_exists($event, 'getEntityManager')) {
+                    $objectManager = $event->getEntityManager();
+                } else {
+                    $objectManager = $event->getDocumentManager();
+                }
+
+                $evtManager = $objectManager->getEventManager();
+                foreach ($evtManager->getListeners() as $listeners) {
+                    foreach ($listeners as $listener) {
+                        if ($listener instanceof BlameableListener) {
+                            $this->blameableListener = $listener;
+
+                            continue;
+                        }
+                        if ($listener instanceof LoggableListener) {
+                            $this->loggableListener = $listener;
+
+                            continue;
+                        }
                     }
                 }
-            }
 
-            if (null != $this->blameableListener) {
-                $this->blameableListener->setUserValue($identity);
-                $evtManager->addEventSubscriber($this->blameableListener);
-            }
+                if (null != $this->blameableListener) {
+                    $this->blameableListener->setUserValue($identity);
+                    $evtManager->addEventSubscriber($this->blameableListener);
+                }
 
-            if (null != $this->loggableListener) {
-                $this->loggableListener->setUsername($identity->getId());
-                $evtManager->addEventSubscriber($this->loggableListener);
-            }
-
+                if (null != $this->loggableListener) {
+                    $this->loggableListener->setUsername($identity->getId());
+                    $evtManager->addEventSubscriber($this->loggableListener);
+                }
+            }            
+        } catch (\Exception $ex) {
+            return;
         }
+
     }
 
 }
