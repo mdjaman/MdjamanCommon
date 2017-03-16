1eb3a222fbffc1fc3615649c5e5a6eb599362bea
Marcel Djaman <marceldjaman@gmail.com>
2017-03-16 10:50:38 +0000

    Added new service

--- src/MdjamanCommon/Service/Factory/LogEntryServiceFactory.php
+++ src/MdjamanCommon/Service/Factory/LogEntryServiceFactory.php
@@ -2,7 +2,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Marcel Djaman
+ * Copyright (c) 2017 Marcel Djaman
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,22 +23,27 @@
  * SOFTWARE.
  */
 
-namespace MdjamanCommon;
+namespace MdjamanCommon\Service\Factory;
 
-return array(
-    'service_manager' => array(
-        'factories' => array(
-            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
-            'MdjamanCommon\Service\LogEntry' => Service\Factory\LogEntryServiceFactory::class
-        ),
-        'invokables' => array(
-            'mdjaman_event_manager'   => 'Zend\EventManager\SharedEventManager',
-            'Form\Upload'  => Form\UploadForm::class,
-        ),
-    ),
-    'controller_plugins' => array(
-        'invokables' => array(
-            'translate' => Controller\Plugin\Translate::class,
-        ),
-    ),
-);
+use MdjamanCommon\Service\LogEntryService;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class LogEntryServiceFactory
+ * @package MdjamanCommon\Service\Factory
+ * @author Marcel Djaman <marceldjaman@gmail.com>
+ */
+class LogEntryServiceFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceLocatorInterface $sl
+     * @return LogEntryService
+     */
+    public function createService(ServiceLocatorInterface $sl)
+    {
+        $om = $sl->get('doctrine.entitymanager.orm_default');
+        return new LogEntryService($sl, $om);
+    }
+
+}
